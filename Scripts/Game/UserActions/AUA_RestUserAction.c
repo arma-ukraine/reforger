6f15c51755c3ea527767600e5b8dc7dee4916d2f
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2025-09-01 12:17:10 +0200

    Cook and rest only on burning fireplaces

--- Scripts/Game/UserActions/AUA_RestUserAction.c
+++ Scripts/Game/UserActions/AUA_RestUserAction.c
@@ -1,10 +1,23 @@
 class AUA_RestUserAction : ScriptedUserAction
 {
+	protected SCR_FireplaceComponent m_FireplaceComponent;
+	
+	//------------------------------------------------------------------------------------------------
+	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
+	{
+		m_FireplaceComponent = SCR_FireplaceComponent.Cast(pOwnerEntity.FindComponent(SCR_FireplaceComponent));
+	}
 
 	//------------------------------------------------------------------------------------------------	
 	override bool CanBeShownScript(IEntity user)
 	{
-		return user;
+		if (!user)
+			return false;
+		
+		if (!m_FireplaceComponent)
+			return false;
+		
+		return m_FireplaceComponent.IsOn();
 	}
 	
 	//------------------------------------------------------------------------------------------------
